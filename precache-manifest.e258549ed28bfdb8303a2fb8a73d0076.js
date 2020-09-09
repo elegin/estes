2399c25b299128396ce7b1400417dfdcc5e47558
Aaron Steimle <aarons@glyphip.com>
2020-09-09 08:43:32 -0400

    Updates

---
+++ precache-manifest.e258549ed28bfdb8303a2fb8a73d0076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed3d7a7102fff2f6906e5faa8e874fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3d223cfef7beca14a3c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a99a2d8dc0f0aa6cf9aa",
+    "url": "/static/js/2.49bc1647.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49bc1647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d223cfef7beca14a3c",
+    "url": "/static/js/main.faf0f93a.chunk.js"
+  },
+  {
+    "revision": "da609625e92379164e3b",
+    "url": "/static/js/runtime-main.3affd6d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
